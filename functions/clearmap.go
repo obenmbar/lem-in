690ecd63d52431782c75ccd529528bcd52f14a28
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-23 08:34:44 +0100

    modification

--- functions/clearmap.go
+++ functions/clearmap.go
@@ -1,14 +1,19 @@
 package lemino
 
-func (bfs *Bfs) Clearmap(G *Graph, S StartEnd){
-		   for _, i := range bfs.Chemin[bfs.IndexChemin][0].link {
-			if len(bfs.Chemin[bfs.IndexChemin]) > 1 {
-				   if i == bfs.Chemin[bfs.IndexChemin][1] {
-				return
-			  }
-		   }
-			}
-		
-          delete(bfs.Chemin, bfs.IndexChemin)
+func (bfs *Bfs) Clearmap(G *Graph, S StartEnd) {
+	if len(bfs.Chemin) >= 2 {
+		if len(bfs.Chemin[0]) == 2 && len(bfs.Chemin[1]) == 2 {
+			delete(bfs.Chemin, 0)
+		}
 	}
 
+	for _, i := range bfs.Chemin[bfs.IndexChemin][0].link {
+		if len(bfs.Chemin[bfs.IndexChemin]) > 1 {
+			if i == bfs.Chemin[bfs.IndexChemin][1] {
+				return
+			}
+		}
+	}
+
+	delete(bfs.Chemin, bfs.IndexChemin)
+}
