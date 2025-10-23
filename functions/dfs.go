fddb4604f3fbaeb9030e056afb2d6d59741fe321
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-23 08:38:16 +0100

    modification

---
+++ functions/dfs.go
@@ -0,0 +1,37 @@
+ package lemino
+
+// import "fmt"
+
+// func (bfs *Bfs) Dfs(rom *Room, Stend StartEnd, G *Graph) bool {
+
+// 	if rom == G.Graphs[Stend.End.Name] {
+
+		
+// 		fmt.Println("trouve start apres return d'abord")
+// 		return true
+// 	 }
+	 
+//    bfs.visitchemin[rom] = true
+ 
+
+// for _, va := range rom.link {
+// 		if len(bfs.Chemin[bfs.IndexChemin]) == 0 {
+// 			bfs.Chemin[bfs.IndexChemin] = append(bfs.Chemin[bfs.IndexChemin], G.Graphs[Stend.End.Name])
+// 		}
+	
+		
+// 			if !bfs.visitchemin[va] {
+// 				bfs.Chemin[bfs.IndexChemin] = append(bfs.Chemin[bfs.IndexChemin], va)
+				
+ 
+// 				if bfs.CHemin(va, Stend, G){
+//                          return true
+// 				}
+			
+// 			}
+		
+// 	}
+	
+// 	return false
+	
+// }
