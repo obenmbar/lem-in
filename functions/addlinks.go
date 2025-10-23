afee8e2a49e005c85e23f5a6a52ef8e36d5a7b75
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-23 08:34:03 +0100

    modification

--- functions/addlinks.go
+++ functions/addlinks.go
@@ -2,7 +2,7 @@ package lemino
 
 import "fmt"
 
-func (G *Graph) AddLinks(link []string) error {
+func (G *Graph) AddLinks(link []string, stend StartEnd) error {
 	if len(link) != 2 {
 		return fmt.Errorf("error en add link ,le number de room n'est pas valable")
 	}
@@ -21,12 +21,12 @@ func (G *Graph) AddLinks(link []string) error {
 
 	if len(G.Graphs[link[0]].link) > 0 {
 		for _, val := range G.Graphs[link[0]].link {
-			if key := G.Graphs[val.Name]; key.Name == link[1]{
+			if key := G.Graphs[val.Name]; key.Name == link[1] {
 				return fmt.Errorf("Error en add link , les deux room deja liee avec eux")
 			}
 		}
 	}
-          
+
 	G.Graphs[link[0]].link = append(G.Graphs[link[0]].link, G.Graphs[link[1]])
 	G.Graphs[link[1]].link = append(G.Graphs[link[1]].link, G.Graphs[link[0]])
 
