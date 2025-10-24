6936bf781bd012beec64f30a7b20fb6e8651dcbc
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-24 09:27:48 +0100

    ajoute des  modifications

--- functions/destributin.go
+++ functions/destributin.go
@@ -3,34 +3,29 @@ package lemino
 import "fmt"
 
 func (bfs *Bfs) Destribuer() {
-	ants_chemin := make([]int, 0, len(bfs.Chemin))
+	ants_chemin := make([]int, len(bfs.Chemin))
 	TotaleAnts := NumberAnts
 	if len(bfs.Chemin) == 1 {
-		ants_chemin = append(ants_chemin, NumberAnts)
-	} else  {
+		ants_chemin[0] = NumberAnts
+	} else {
 		for TotaleAnts > 0 {
-		
-			fmt.Println(len(bfs.Chemin))
 			for i := 0; i < len(bfs.Chemin)-1; i++ {
-				
-				fmt.Println(i)
-				if i+1 < len(ants_chemin){
-					if (len(bfs.Chemin[i]) + ants_chemin[i]) > (len(bfs.Chemin[i+1]) + ants_chemin[i+1]) {
-					fmt.Println("hh")
+				if TotaleAnts == 0 {
+					break
+				}
+
+				if (len(bfs.Chemin[i]) + ants_chemin[i]) > (len(bfs.Chemin[i+1]) + ants_chemin[i+1]) {
 					TotaleAnts--
 					ants_chemin[i+1]++
-					break
+
 				} else {
-            
 					TotaleAnts--
 					ants_chemin[i]++
-					break
+
 				}
-				}
-				TotaleAnts--
 			}
 		}
-	} 
+	}
 
 	fmt.Println(ants_chemin)
 	bfs.Affichage(ants_chemin)
