843d6b901f77fedfbfe78ddabb479dc5ad620990
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-24 09:29:11 +0100

    ajoute l'algorithme  de DFS QUANT EN TRAVAIL SUR LES GRANDS FILE CAR IL REDUIRE LA MEMOIRE PLUS bfs

--- functions/dfs.go
+++ functions/dfs.go
@@ -1,37 +1,23 @@
- package lemino
+package lemino
 
-// import "fmt"
+import "fmt"
 
-// func (bfs *Bfs) Dfs(rom *Room, Stend StartEnd, G *Graph) bool {
 
-// 	if rom == G.Graphs[Stend.End.Name] {
 
+func (G *Graph) Dfs(rom *Room, Stend StartEnd, BFS *Bfs)  {
+    
+	if rom == G.Graphs[Stend.End.Name]{
 		
-// 		fmt.Println("trouve start apres return d'abord")
-// 		return true
-// 	 }
-	 
-//    bfs.visitchemin[rom] = true
- 
-
-// for _, va := range rom.link {
-// 		if len(bfs.Chemin[bfs.IndexChemin]) == 0 {
-// 			bfs.Chemin[bfs.IndexChemin] = append(bfs.Chemin[bfs.IndexChemin], G.Graphs[Stend.End.Name])
-// 		}
-	
+		fmt.Println("il trouve un chemin ")
+		BFS.CHemin(rom,Stend,G)
+		return 
+	}
+   BFS.Visted[rom]= true
+    for _, room := range rom.link {
+		if !BFS.Visted[room] {
+   G.Dfs(room,Stend,BFS) 
 		
-// 			if !bfs.visitchemin[va] {
-// 				bfs.Chemin[bfs.IndexChemin] = append(bfs.Chemin[bfs.IndexChemin], va)
-				
- 
-// 				if bfs.CHemin(va, Stend, G){
-//                          return true
-// 				}
-			
-// 			}
-		
-// 	}
-	
-// 	return false
-	
-// }
+	  
+		}
+	}
+}
