2206b45a2231f25ee4cd775abbce5e778f63d137
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-25 14:00:14 +0100

    ajoute des modification et des commentaire

--- functions/bfs.go
+++ functions/bfs.go
@@ -1,48 +1,42 @@
 package lemino
-
-import "fmt"
-
+// BFS performs a Breadth-First Search on the graph to find all possible paths
+// from the start room to the end room, and then distributes ants through those paths.
 func (G *Graph) BFS(Stend StartEnd) {
-	var bfs Bfs = Bfs{Visted: make(map[*Room]bool), Chemin: make(map[int][]*Room), visitchemin: make(map[*Room]bool)}
-	if len(G.Graphs)>1000{
-		G.Dfs(G.Graphs[Stend.Start.Name],Stend,&bfs)
+	var bfs Bfs = Bfs{Visted: make(map[string]bool), Chemin: make(map[int][]string), visitchemin: make(map[string]bool)}
+	if len(G.Graphs) > 1000 {
+		G.Dfs(Stend.Start, Stend, &bfs)
 		delete(bfs.Chemin, bfs.IndexChemin)
-	    bfs.Reverse()
+		bfs.Reverse()
 		bfs.Destribuer()
 		return
 	}
-	
-	queue := make([]*Room, 0, len(G.Graphs))
-	queue = append(queue, G.Graphs[Stend.Start.Name])
 
+	queue := make([]string, 0, len(G.Graphs))
+	queue = append(queue, Stend.Start)
 	for len(queue) > 0 {
+
 		rom := queue[0]
 
 		queue = queue[1:]
 
 		bfs.Visted[rom] = true
 
-		if rom == G.Graphs[Stend.End.Name] {
-			bfs.CHemin(G.Graphs[Stend.End.Name], Stend, G)
-			fmt.Println("OFIGHT")
+		if rom == Stend.End {
+			bfs.CHemin(Stend.End, Stend, G)
+
 			continue
 		}
 
-		for _, val := range rom.link {
+		for _, val := range G.Graphs[rom].link {
 			if !bfs.Visted[val] {
 				queue = append(queue, val)
+				continue
 			}
 		}
 	}
 
 	delete(bfs.Chemin, bfs.IndexChemin)
 	bfs.Reverse()
-	for l, val := range bfs.Chemin {
-		fmt.Println(l)
-		for _, i := range val {
-			fmt.Println(i)
-		}
-	}
- 
+
 	bfs.Destribuer()
 }
