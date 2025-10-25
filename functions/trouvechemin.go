3896d35221cd239551d44964cdf0d84ddeef8e12
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-25 14:02:25 +0100

    ajoute des modification et des commentaire

--- functions/trouvechemin.go
+++ functions/trouvechemin.go
@@ -1,47 +1,37 @@
 package lemino
 
-import "fmt"
-
-func (bfs *Bfs) CHemin(rom *Room, Stend StartEnd, G *Graph) bool {
-
-	if rom == G.Graphs[Stend.Start.Name] {
-		bfs.Visted[G.Graphs[Stend.Start.Name]] = false
-		bfs.Visted[G.Graphs[Stend.End.Name]] = false
-	       bfs.Clearmap(G, Stend)
-		   bfs.IndexChemin++
-		fmt.Println("trouve start apres return d'abord")
+// 🔁 CHemin cherche récursivement un chemin entre la salle de fin et celle du début
+// Elle enregistre chaque chemin valide dans bfs.Chemin
+func (bfs *Bfs) CHemin(rom string, Stend StartEnd, G *Graph) bool {
+	if rom == Stend.Start {
+		bfs.Visted[Stend.Start] = false
+		bfs.Visted[Stend.End] = false
+		bfs.Clearmap(G, Stend)
+		bfs.IndexChemin++
 		return true
-	 }
-	 
-   bfs.visitchemin[rom] = true
- 
+	}
 
-for _, va := range rom.link {
+	bfs.visitchemin[rom] = true
+
+	for _, va := range G.Graphs[rom].link {
 		if len(bfs.Chemin[bfs.IndexChemin]) == 0 {
-			bfs.Chemin[bfs.IndexChemin] = append(bfs.Chemin[bfs.IndexChemin], G.Graphs[Stend.End.Name])
+			bfs.Chemin[bfs.IndexChemin] = append(bfs.Chemin[bfs.IndexChemin], G.Graphs[Stend.End].Name)
 		}
-		// fmt.Println("==>", rom.Name, "links to:")
-		// for _, v := range rom.link {
-		// 	fmt.Println("   -", v.Name)
-		// }
 
 		if _, ok := bfs.Visted[va]; ok {
 			if !bfs.visitchemin[va] {
-				  bfs.visitchemin[rom] = true
+				bfs.visitchemin[rom] = true
 				bfs.Chemin[bfs.IndexChemin] = append(bfs.Chemin[bfs.IndexChemin], va)
-				
- 	               if bfs.CHemin(va, Stend, G){
-                         return true
-				   }else {
+
+				if bfs.CHemin(va, Stend, G) {
+					return true
+				} else {
 					bfs.Chemin[bfs.IndexChemin] = bfs.Chemin[bfs.IndexChemin][:len(bfs.Chemin[bfs.IndexChemin])-1]
 					bfs.visitchemin[rom] = false
-				   }
-				
-		
+				}
 			}
 		}
 	}
-	
+
 	return false
-	
 }
