aba1654aa39db6d2c1b792c1f4e249377faa03cf
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-25 14:01:58 +0100

    ajoute des modification et des commentaire

--- functions/reverse.go
+++ functions/reverse.go
@@ -1,5 +1,6 @@
 package lemino
-
+// 🔄 Reverse inverse l'ordre des salles dans chaque chemin trouvé par BFS
+// Elle permet de passer d'un chemin "fin → début" à un chemin "début → fin"
 func (bfs *Bfs)Reverse(){
 	for _, value := range bfs.Chemin {
 		for k := 0 ; k < len(value)/2 ; k++ {
