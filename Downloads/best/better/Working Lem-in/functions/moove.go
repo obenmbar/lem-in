f938e1bb77baec00a7867c2f14436b7cbca995f5
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-29 08:33:42 +0100

    ajoute project qui trouve tous les chemind de start a end utilisont l'algorithme d'edmonds-karp qui resoudre le problemme de max flow mais qunt tu trouve le max floww alors tu trouve tous les chemin disjoinct

---
+++ Downloads/best/better/Working Lem-in/functions/moove.go
@@ -0,0 +1,53 @@
+package functions
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func MooveAnts(paths []Path, antNumber int, data string, assigned []int) {
+	fmt.Println(strings.TrimSpace(data))
+	fmt.Println("")
+
+	ants := make([]Ant, 0)
+	finished := 0
+	ID := 1
+
+	for finished < antNumber {
+		mooves := []string{}
+
+		for i := range ants {
+			ant := &ants[i]
+			last := len(ant.Path) - 1
+
+			if ant.Position < last {
+				ant.Position++
+				mooves = append(mooves, ant.Name+"-"+ant.Path[ant.Position])
+
+			}
+			if ant.Position == last && !ant.Finished {
+				ant.Finished = true
+				finished++
+			}
+		}
+
+		for i, path := range paths {
+			if assigned[i] > 0 {
+				newAnt := Ant{
+					Name:     "L" + strconv.Itoa(ID),
+					Path:     path,
+					Position: 0,
+				}
+				ants = append(ants, newAnt)
+				mooves = append(mooves, newAnt.Name+"-"+newAnt.Path[0])
+				ID++
+				assigned[i]--
+			}
+		}
+
+		if len(mooves) > 0 {
+			fmt.Println(strings.Join(mooves, " "))
+		}
+	}
+}
