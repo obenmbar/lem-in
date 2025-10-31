f15d837c7dbb18d8cb1844474b16bb15a766f63e
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2025-10-31 14:18:44 +0100

    ajoute des modification sur la func qui trouve les chemin

--- functions/trouvechemin.go
+++ functions/trouvechemin.go
@@ -2,36 +2,37 @@ package lemino
 
 // 🔁 CHemin cherche récursivement un chemin entre la salle de fin et celle du début
 // Elle enregistre chaque chemin valide dans bfs.Chemin
-// func (bfs *Bfs) CHemin(rom string, Stend StartEnd, G *Graph) bool {
-// 	if rom == Stend.Start {
-// 		bfs.Visted[Stend.Start] = false
-// 		bfs.Visted[Stend.End] = false
-// 		bfs.Clearmap(G, Stend)
-// 		bfs.IndexChemin++
-// 		return true
-// 	}
+func (bfs *Bfs) CHemin(rom string, Stend StartEnd, G *Graph) bool {
+	if rom == Stend.Start {
+		bfs.Visted[Stend.Start] = false
+		bfs.Visted[Stend.End] = false
+		bfs.Clearmap(G, Stend)
+		bfs.IndexChemin++
+		return true
+	}
 
-// 	bfs.visitchemin[rom] = true
+	bfs.visitchemin[rom] = true
 
-// 	for _, va := range G.Graphs[rom].link {
-// 		if len(bfs.Chemin[bfs.IndexChemin]) == 0 {
-// 			bfs.Chemin[bfs.IndexChemin] = append(bfs.Chemin[bfs.IndexChemin], G.Graphs[Stend.End].Name)
-// 		}
+	for _, va := range G.Graphs[rom].link {
+		if len(bfs.Chemin[bfs.IndexChemin]) == 0 {
+			bfs.Chemin[bfs.IndexChemin] = append(bfs.Chemin[bfs.IndexChemin], G.Graphs[Stend.End].Name)
+		}
 
-// 		if _, ok := bfs.Visted[va]; ok {
-// 			if !bfs.visitchemin[va] {
-// 				bfs.visitchemin[rom] = true
-// 				bfs.Chemin[bfs.IndexChemin] = append(bfs.Chemin[bfs.IndexChemin], va)
+		if _, ok := bfs.Visted[va]; ok {
+			if !bfs.visitchemin[va] {
+				bfs.visitchemin[rom] = true
+				bfs.Chemin[bfs.IndexChemin] = append(bfs.Chemin[bfs.IndexChemin], va)
 
-// 				if bfs.CHemin(va, Stend, G) {
-// 					return true
-// 				} else {
-// 					bfs.Chemin[bfs.IndexChemin] = bfs.Chemin[bfs.IndexChemin][:len(bfs.Chemin[bfs.IndexChemin])-1]
-// 					bfs.visitchemin[rom] = false
-// 				}
-// 			}
-// 		}
-// 	}
+				if bfs.CHemin(va, Stend, G) {
+					return true
+					
+				} else {
+					bfs.Chemin[bfs.IndexChemin] = bfs.Chemin[bfs.IndexChemin][:len(bfs.Chemin[bfs.IndexChemin])-1]
+					bfs.visitchemin[rom] = false
+				}
+			}
+		}
+	}
 
-// 	return false
-// }
+	return false
+}
